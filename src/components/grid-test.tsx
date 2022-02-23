0412396e368b1751f3260cff068c94523b1ed22f
Gabriel Shannon <gabrielshannon96@gmail.com>
2022-02-23 19:57:35 +0000

    new mobile version

--- src/components/grid-test.tsx
+++ src/components/grid-test.tsx
@@ -16,35 +16,49 @@ export function Grid() {
   return (
     <div style={{}}>
       <div className="container">
+        <div className="hello-full-page">
+          <h1>PREMIERE: baccy beard - 4head [Slump Sounds]</h1>
+          <h1>4head</h1>
+          <h1>Job Type - Numbers Vol​.​3
+          </h1>
+          <h1>baccy beard - nearest@exomoon
+          </h1>
+          <h1>SolidMixtape1
+            by Lydios Materials Inc.</h1>
+          <h1>SLUMP SOUNDS // LATITUDE FESTIVAL</h1>
+          <h1>Instagram</h1>
+          <h1>Facebook</h1>
+          
+        </div>
         <div className="LT">
           {/* <img className="anime" src={anime} width="15%"/> */}
         </div>
 
         <div className="nested_content">
           <div className="nested_header">
-          
+
             {" "}
             {/* <Marquee gradient={false}> */}
-              <img className="banner" src={tempLOGO}/>
+            <img className="banner" src={tempLOGO} />
             {/* </Marquee> */}
-          {/* </div> */}
+            {/* </div> */}
           </div>
 
           <div className="nested_mid1">
-          <div className="myMarque">
-            <Marquee speed={50} gradient={false}>
-              www.slumpsounds.co.uk &nbsp; &nbsp; &nbsp; &nbsp; 0113/SE &nbsp;
-              &nbsp; &nbsp; &nbsp; contact/bookings: slumpsounds@outlook.com
-              &nbsp; &nbsp; &nbsp; &nbsp; Leeds, UK &nbsp; &nbsp; &nbsp; &nbsp;
-              www.slumpsounds.co.uk &nbsp; &nbsp; &nbsp; &nbsp; 0113/SE &nbsp;
-              &nbsp; &nbsp; &nbsp; contact/bookings: slumpsounds@outlook.com
-              &nbsp; &nbsp; &nbsp; &nbsp; Leeds, UK &nbsp; &nbsp; &nbsp; &nbsp;
-              www.slumpsounds.co.uk &nbsp; &nbsp; &nbsp; &nbsp; 0113/SE &nbsp;
-              &nbsp; &nbsp; &nbsp; contact/bookings: slumpsounds@outlook.com
-              &nbsp; &nbsp; &nbsp; &nbsp; Leeds, UK &nbsp; &nbsp; &nbsp; &nbsp;
-            </Marquee>
+            <div className="myMarque">
+              <Marquee speed={50} gradient={false}>
+                www.slumpsounds.co.uk &nbsp; &nbsp; &nbsp; &nbsp; 0113/SE &nbsp;
+                &nbsp; &nbsp; &nbsp; contact/bookings: slumpsounds@outlook.com
+                &nbsp; &nbsp; &nbsp; &nbsp; Leeds, UK &nbsp; &nbsp; &nbsp; &nbsp;
+                www.slumpsounds.co.uk &nbsp; &nbsp; &nbsp; &nbsp; 0113/SE &nbsp;
+                &nbsp; &nbsp; &nbsp; contact/bookings: slumpsounds@outlook.com
+                &nbsp; &nbsp; &nbsp; &nbsp; Leeds, UK &nbsp; &nbsp; &nbsp; &nbsp;
+                www.slumpsounds.co.uk &nbsp; &nbsp; &nbsp; &nbsp; 0113/SE &nbsp;
+                &nbsp; &nbsp; &nbsp; contact/bookings: slumpsounds@outlook.com
+                &nbsp; &nbsp; &nbsp; &nbsp; Leeds, UK &nbsp; &nbsp; &nbsp; &nbsp;
+              </Marquee>
 
-                </div>
+            </div>
             {/* <img className="title" src={tempLOGO} width="15%" /> */}
             {/* <img className="flame" src={flame} width="40px" />
             <img className="headerBg" src={bgIm} width="90%" />
@@ -52,6 +66,7 @@ export function Grid() {
           </div>
 
           <div className="nested_app">
+
             <PlayerApp />
           </div>
         </div>
