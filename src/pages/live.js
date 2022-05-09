0251b828bde168df7791e7a38fa1770cf9c4e01e
Gabriel Shannon <gabrielshannon96@gmail.com>
2022-05-09 21:00:28 +0100

    live page

---
+++ src/pages/live.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import Livestream from "../components/livestreamfeed"
+
+const SecondPage = () => (
+  <div>
+    <Livestream></Livestream>
+  </div>
+)
+
+export default SecondPage
